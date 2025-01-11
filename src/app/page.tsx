633e1fa49359098b8954ec638dd341d6c37337de
Avinav Bhandari <avinavbhandari@gmail.com>
2025-01-11 15:12:31 -0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,160 @@
-import Image from "next/image";
+import React from 'react';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* FIRST SCENE: 5X1 + BOT */}
+      <div className="relative w-[1920px] h-[1080px] bg-green-400 overflow-hidden mb-12">
+        <div className='absolute top-0 left-0 w-[1920px] h-[400px] bg-green-400 overflow-hidden z-10'>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        {/* 5 X 1 INDIVUDAL VIDEOS */}
+        <div className="w-[300px] h-[1000px] absolute bottom-0 right-0 bg-green-500 overflow-hidden">
+          <div className="w-[600px] overflow-hidden">
+            <video
+              className="w-[450px] h-[1000px] object-cover scale-[1] translate-x-[-20px]"
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source src="/single.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+        <div className="w-[300px] h-[1000px] absolute bottom-0 right-[300px] bg-green-500 overflow-hidden">
+          <div className="w-[600px] overflow-hidden">
+            <video
+              className="w-[450px] h-[1000px] object-cover scale-[1] translate-x-[-20px]"
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source src="/single.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+        <div className="w-[300px] h-[1000px] absolute bottom-0 right-[600px] bg-green-500 overflow-hidden">
+          <div className="w-[600px] overflow-hidden">
+            <video
+              className="w-[450px] h-[1000px] object-cover scale-[1] translate-x-[-20px]"
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source src="/single.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+        <div className="w-[300px] h-[1000px] absolute bottom-0 right-[900px] bg-green-500 overflow-hidden">
+          <div className="w-[600px] overflow-hidden">
+            <video
+              className="w-[450px] h-[1000px] object-cover scale-[1] translate-x-[-20px]"
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source src="/single.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+        <div className="w-[300px] h-[1000px] absolute bottom-0 right-[1200px] bg-green-500 overflow-hidden">
+          <div className="w-[600px] overflow-hidden">
+            <video
+              className="w-[450px] h-[1000px] object-cover scale-[1] translate-x-[-20px]"
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source src="/single.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+        {/* BOT VIDEO */}
+        <div className='absolute top-[400px] left-0 z-20 w-[420px] overflow-hidden'>
+          <video 
+            className='w-[450px] h-[300px] object-cover'
+            autoPlay
+            loop
+            playsInline
+            muted
+          >
+            <source src="/3388N.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      
+
+      {/* SECOND SCENE: 1X3 + BOT */}
+      <div className="relative w-[1920px] h-[1080px] bg-green-400 overflow-hidden mb-12">
+
+        {/* 1 X 3 INDIVUDAL VIDEO */}
+        <div className="w-[1300px] h-[1000px] absolute bottom-0 right-0 bg-green-500 overflow-hidden">
+          <div className="w-[1300px] overflow-hidden">
+            <video
+              className="w-[1300px] h-[1000px] object-cover scale-[1]"
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source src="/triple.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+
+        {/* BOT VIDEO */}
+        <div className='absolute top-[400px] left-[0] z-20 w-[640px] overflow-hidden'>
+          <video 
+            className='w-[640px] h-[600px] object-cover'
+            autoPlay
+            loop
+            playsInline
+            muted
+          >
+            <source src="/3388N.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
+
+
+      {/* THIRD SCENE: 1X6 + BOT */}
+      <div className="relative w-[1920px] h-[1080px] bg-green-400 overflow-hidden mb-12">
+
+        {/* 1 X 6 INDIVUDAL VIDEO */}
+        <div className="w-[1300px] absolute bottom-0 right-0 bg-green-500 overflow-hidden">
+          <div className="w-[1300px] overflow-hidden">
+            <video
+              className="w-[1300px] object-cover scale-[1]"
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source src="/six.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+        
+        {/* BOT VIDEO */}
+        <div className='absolute top-[400px] left-[0] z-20 w-[640px] overflow-hidden'>
+          <video 
+            className='w-[640px] h-[600px] object-cover'
+            autoPlay
+            loop
+            playsInline
+            muted
+          >
+            <source src="/3388N.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
